cfb47d6731af19dfb132e324070220c8e7744f9e
beloner <belone00@gmail.com>
2023-07-05 19:20:31 -0300

    asd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,15 +8,22 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css',])
 </head>
 <body class="layout-body">
-<nav class="navbar navbar-expand-md">
+<nav class="navbar navbar-expand-md mt-2">
     <div class="container">
-        <div class=" mt-3 collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav me-auto" alt=""><h3 class="fw-bolder">joao belone</h3></ul>
-            <!-- Right Side Of Navbar -->
-            <ul class="nav2"><a href="/" class="fw-bolder">About</a></ul>
-            <ul class="nav2"><a href="/" class="fw-bolder">Projects</a></ul>
-            <ul class="nav2"><a href="/" class="fw-bolder">Contact</a></ul>
+        <ul class="navbar-nav me-auto" alt=""><h3 class="fw-bolder">joao belone</h3></ul>
+        <button class=" navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512">
+                <path
+                    d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
+            </svg>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav2"><a href="/" class="fw-bolder">About</a></li>
+                <li class="nav2"><a href="/" class="fw-bolder">Projects</a></li>
+                <li class="nav2"><a href="/" class="fw-bolder">Contact</a></li>
+            </ul>
         </div>
     </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,4 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Auth::routes();
+Route::get('/home', [HomeController::class, 'index'])->name('home');
