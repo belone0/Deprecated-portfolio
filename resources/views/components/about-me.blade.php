b223c7a63d4d313d826d161cd48eaf8fcb3a262b
beloner <belone00@gmail.com>
2023-07-05 22:38:22 -0300

    tech stack e componentes

---
+++ resources/views/components/about-me.blade.php
@@ -0,0 +1,21 @@
+ <div class="col-xl-6 col-12 text-section p-4 p-lg-0">
+        <p>
+            Hi! I'm João. Ever since I was a child, I always liked technology, playing video games and fiddling
+            with the terminals, which made
+            me
+            gravitate towards coding. Today, I am currently studying Computer Science and working as a
+            Fullstack Web Developer at <a href="">OPCTEC</a>.
+        </p>
+        <p>
+            I've had the privilege of building web and mobile solutions for a platform that has more than 100
+            thousand users,
+            working as a team
+            to deliver the best, as fast as possible. I've also participated in several hackathons, coding,
+            building and presenting projects against the time.
+        </p>
+        <p>
+            When I'm not working or studying, I'm playing guitar, working out, reading some book or playing
+            video games.
+        </p>
+    </div>
+
