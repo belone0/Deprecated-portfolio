3cf2f36cf53fe7e88ac5609ad9f43170e58097b0
beloner <belone00@gmail.com>
2023-07-07 19:05:14 -0300

    scroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 import './bootstrap';
+
+//firefox scroll
+var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+if (isFirefox) {
+    var scrollbarTrack = document.querySelector('body');
+    scrollbarTrack.style.background = '#0F172A';
+}
+
+//nav scroll glass
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+
+    //when scrolls
+    if (window.scrollY > 0) {
+        navbar.classList.remove('w-75', 'col-9', 'border-0');
+        navbar.classList.add('glass', 'col-6', 'rounded', 'rounded-pill', 'transparent-border');
+    } else {
+        navbar.classList.add('w-75', 'border-0', 'transparent-border');
+        navbar.classList.remove('glass', 'col-6', 'col-9', 'rounded', 'rounded-pill');
+    }
+});
+
+
